4019fcbba24ada4bdaaa55c62f111da7f29c5945
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2025-01-20 19:32:05 -1200

    added next-ui, lucid icons to project, and create start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { Button } from "@heroui/react"
+import { CirclePlay } from "lucide-react"
 
 
 function App() {
 
   return (
-    <>
-      <div>
-     
+    <div
+      className="bg-slate-600 md:w-[768px] h-[668px] lg:w-[1024px] m-auto flex justify-center items-center"
+    >
+      <div className=" place-items-center">
+        <img src="OXd_0T.png" alt="squid game" width={300} height={200} />
+        <h1 className="text-2xl text-white font-medium">Squid Game Online</h1>
+        <Button color="primary" size="lg" className="text-white text-2xl font-medium">
+          Play Now <CirclePlay />
+        </Button>
       </div>
-    </>
+    </div>
   )
 }
 
