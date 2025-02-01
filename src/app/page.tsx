49dffccf212e08c00290125a33a1b1523511040c
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2025-01-31 17:54:05 -1200

    added (sound): add howler js and music for first start game page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
 import Image from "next/image"
 import {Button, ButtonGroup} from "@heroui/button";
 import Link from "next/link";
+import PinkSoldier from "@/components/sounds/PinkSoldier";
 
 export default function Home() {
  
   return (
-    <div className="min-w-96 sm:min-w-[650px] lg:min-w-[1020px] h-full min-h-[700px] bg-purple-500 place-content-center">
+    <div className="w-full h-screen bg-pink-400 place-content-center">
       <div
-        className="place-content-center place-items-center place-self-center min-h-96 rounded w-[500px] bg-slate-400"
+        className="place-content-center place-items-center place-self-center sm:min-h-96 rounded sm:w-[500px] w-[90%] bg-slate-400 p-6"
       >
         <Image src={"/SquidGame_Season1_Episode1_00_44_44_16.webp"} alt={""} width={400} height={300} />
         <h1 className="text-2xl font-medium text-white my-3">Squid Game Online</h1>
         <Link href={"/red-light-green-light"}>
          <Button color="secondary" size="lg" className="">
           Play now
-    </Button></Link>
+          </Button></Link>
+        <PinkSoldier/>
       </div>
    </div>
   )
---
+++ src/components/sounds/PinkSoldier.tsx
@@ -0,0 +1,25 @@
+"use client"
+import { useEffect, useState } from "react";
+import { Howl } from "howler";
+
+const PinkSoldier = () => {
+
+   useEffect(() => {
+    // Initialize the sound
+    const sound = new Howl({
+      src: ["/sounds/SQUAD GAME OST -  season 1 (320).mp3"], // Path to your background music file in the public folder
+      volume: 1, // Adjust volume (0.0 to 1.0)
+      loop: true, // Loop the background music
+      autoplay: true, // Automatically play the music when the page loads
+    });
+
+    // Cleanup
+    return () => {
+      sound.unload(); // Unload the sound when the component unmounts
+    };
+   }, []);
+  
+    return(<></>)
+}
+
+export default PinkSoldier
