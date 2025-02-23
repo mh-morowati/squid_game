38f070db708cbb421315968be7a779c899efb97b
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2025-02-23 05:24:53 -1200

    test: button conflict in touch screens with mouse

--- src/components/Finish.tsx
+++ src/components/Finish.tsx
@@ -35,14 +35,12 @@ const Finish = (props: Props) => {
                       alt={""} 
                      fill
                      />
-                    <Link href={"/"}>
                         <Button
                             className="mt-4 min-[2000px]:p-5 min-[2000px]:w-60 min-[2000px]:h-20 min-[2000px]:text-3xl"
                             onPress={() => {setGameStarted(false),router.push("/")}}
                         >
                         Restart Game
                         </Button>
-                    </Link>
                 </ModalContent>
                
   </Modal>
