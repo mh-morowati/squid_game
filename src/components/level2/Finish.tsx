ad237b3ac3ec0082843c5481e86f2ab1b4d97b20
mh-morowati <162552049+mh-morowati@users.noreply.github.com>
2025-07-17 01:05:46 -1200

    refactor:level2 finish
    add back to first game

--- src/components/level2/Finish.tsx
+++ src/components/level2/Finish.tsx
@@ -2,6 +2,7 @@ import { useMarbleGameStore } from "@/store/marbleGameStore"
 import { Button } from "@heroui/button"
 import {  Modal,  ModalContent} from "@heroui/modal"
 import Image from 'next/image'
+import Link from "next/link"
 
 
 const Finish = () => {
@@ -37,12 +38,17 @@ const Finish = () => {
                 <ModalContent>
                     <Button
                         onClick={resetGame}
-                        className="absolute z-50 left-1/2 top-10"
+                        className="absolute z-50 sm:left-1/3 top-10"
                         size="lg"
                         color="primary"
                     >
                         Play again
-                    </Button>
+            </Button>
+             <Link
+                        href={"/"}
+                        className="absolute z-50 left-1/2 top-10 text-white bg-green-600 py-3 rounded-xl px-6">
+                        First Game
+                    </Link>
                     <Image src={"/winning-gif.gif"} alt={""} fill />
               </ModalContent>
             </Modal>)}
